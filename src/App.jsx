2a4c005b2b65493a7563607bef5343855dc1fe2c
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-18 16:08:54 +0100

    add: form

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,18 @@ function App() {
     <>
       <AppHeader />
       <div className="container">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="Articolo" className="form-label">Articoli</label>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Digita titolo Articolo" aria-label="Titolo Articolo" aria-describedby="button-addon2" />
+              <button className="btn btn-outline-secondary" type="button" id="button-addon2">Aggiungi</button>
+            </div>
 
+            <small id="articoloHelperId" className="form-text text-muted">Inserisci titolo articolo</small>
+          </div>
+
+        </form>
       </div>
     </>
   )
