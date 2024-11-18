46b1e2a1d77e5430d01a64a0cf2594824876e373
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-18 15:31:11 +0100

    add: header and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-
+import AppHeader from './components/AppHeader'
 import './App.css'
+
 
 function App() {
 
 
   return (
     <>
+      <AppHeader />
+      <div className="container">
 
+      </div>
     </>
   )
 }
