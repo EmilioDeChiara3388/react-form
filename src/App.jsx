5fa7d2caa49fc224dced9b5e2bd519796fc1ad6c
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-18 16:43:33 +0100

    add: new article on submit

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import { useState } from 'react'
 import AppHeader from './components/AppHeader'
 import './App.css'
 
-const articles = []
+const initialArticles = []
 
 function App() {
 
+  const [articles, setArticles] = useState(initialArticles)
+  const [newArticle, setNewArticle] = useState("")
+
+  function addArticle(e) {
+    e.preventDefault()
+    console.log(articles);
+    setArticles([...articles, newArticle])
+  }
 
   return (
     <>
       <AppHeader />
       <div className="container">
-        <form className='my-3'>
+        <form onSubmit={addArticle} className='my-3'>
           <div className="mb-3">
             <label htmlFor="Articolo" className="form-label">Articoli</label>
             <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Digita titolo Articolo" aria-label="Titolo Articolo" aria-describedby="button-addon2" />
+              <input type="text" className="form-control" placeholder="Digita titolo Articolo" aria-label="Titolo Articolo" aria-describedby="button-addon2"
+                value={newArticle} onChange={e => setNewArticle(e.target.value)} />
               <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Aggiungi</button>
             </div>
 
@@ -23,9 +32,7 @@ function App() {
           </div>
         </form>
         <ul className="list-group">
-          <li className="list-group-item">Articolo 1</li>
-          <li className="list-group-item">Articolo 2</li>
-          <li className="list-group-item">Articolo 3</li>
+          {articles.map((article, index) => <li key={index} className="list-group-item">{article}</li>)}
         </ul>
 
       </div>
